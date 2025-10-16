85081ff2260ab6149d65527c116887107b1f86d8
xRevenge06 <ronaldoege123@gmail.com>
2025-10-17 01:27:04 +0300

    backend cors fixed

--- backend/app.js
+++ backend/app.js
@@ -6,12 +6,14 @@ const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
-app.use(cors({
-  origin: ["http://localhost:3001"],
-  credentials: true,
-  methods: ["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
-  allowedHeaders: ["Content-Type","Authorization"],
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:3001", "https://kirazbilisim.vercel.app/"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(cookieParser());
 app.use(express.json());
