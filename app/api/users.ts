15e392384b28c2057398f15caa932a5e6b9a81a6
xRevenge06 <ronaldoege123@gmail.com>
2025-10-16 23:56:26 +0300

    Start of project

--- app/api/users.ts
+++
@@ -1,3 +0,0 @@
-// Bu dosya hafızada kullanıcıları tutacak, server restart olana kadar
-export const users: { name: string; email: string; password: string }[] = [];
-export const dynamic = "force-dynamic"
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'dynamic',
+  output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true,
   },
